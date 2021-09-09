45eaaa763ca83cb961ee6f5c28c621df299e0383
Petit Abba <79740011+Petit-Abba@users.noreply.github.com>
2021-09-10 05:04:55 +0800

    Add files via upload

--- .github/check.sh
+++ .github/check.sh
@@ -5,6 +5,7 @@ sed -i "/^若使用有问题/c 若使用有问题，请建立[issues](https://gi
 Latest_version="$(curl "https://github.com/YAWAsau/backup_script/releases" -sL | awk -F "/YAWAsau/backup_script/releases/download" '{print $2}' | awk -F ".zip" '{print $1}')"
 Latest_version="$(echo ${Latest_version} | awk -F "/" '{print $2}')"
 [ -f "${home}/zip/version" ] && Previous_version="$(cat ${home}/zip/version)" || Previous_version=""
+rm -rf ${home}/zip/v${Previous_version}.zip 1>/dev/null 2>&1
 
 echo "- 当前时间: $(date '+%Y-%m-%d %H:%M:%S')"
 echo "- 原仓库最新版本: v${Latest_version}"
@@ -26,5 +27,3 @@ if [ "${Previous_version}" != "${Latest_version}" ]; then
 else
   echo "- 暂无新版更新"
 fi
-
-rm -rf ${home}/zip/v${Previous_version}.zip 1>/dev/null 2>&1 && echo "- 清除残留zip包 减少资源占用"
